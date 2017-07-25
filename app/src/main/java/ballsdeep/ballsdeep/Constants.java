d480d32a6bb3292788cdd525e9d5238241cd029f
Duarte Dias <DuartexDias@gmail.com>
2017-07-25 15:46:01 +0200

    Ball Physics (with bugs)

--- app/src/main/java/ballsdeep/ballsdeep/Constants.java
+++ app/src/main/java/ballsdeep/ballsdeep/Constants.java
@@ -7,7 +7,7 @@ package ballsdeep.ballsdeep;
 public class Constants {
     public static int SCREEN_WIDTH;
     public static int SCREEN_HEIGHT;
-    public static final float MINSPEED = -20f;
+    public static final float MINSPEED = -21f;
     public static final float MAXSPEED = 20f;
     public static final float AIRSPEED = -7.8f;
     public static final float WALLSPEED = 0.6f;
