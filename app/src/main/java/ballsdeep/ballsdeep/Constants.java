f3d04df9062ffa0ac78dd6c63a94553d9d65184b
Goncalo Marques <goncalo.cool@gmail.com>
2017-07-25 14:23:41 +0100

    Dimensões do ecrã

---
+++ app/src/main/java/ballsdeep/ballsdeep/Constants.java
@@ -0,0 +1,10 @@
+package ballsdeep.ballsdeep;
+
+/**
+ * Created by Goncalo on 25/07/2017.
+ */
+
+public class Constants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+}
